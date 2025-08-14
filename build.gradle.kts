3c484b0ef166cc824a27b8d39f8d86acb80aa06e
sadnex <s4dnex@gmail.com>
2025-08-15 02:57:09 +0300

    build: ⬆️ upgrade Paper version to 1.21.5

--- build.gradle.kts
+++ build.gradle.kts
@@ -23,7 +23,7 @@ repositories {
 }
 
 dependencies {
-    compileOnly("io.papermc.paper:paper-api:1.21.4-R0.1-SNAPSHOT")
+    compileOnly("io.papermc.paper:paper-api:1.21.5-R0.1-SNAPSHOT")
     compileOnly("com.onarandombox.multiversecore:multiverse-core:4.3.14")
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
     compileOnly("org.jetbrains.kotlin:kotlin-stdlib")
